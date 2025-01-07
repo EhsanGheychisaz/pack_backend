4be5aaac4ad548205d8d760e26ddd60fa8f9df2c
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2025-01-07 21:18:12 +0330

    fix buga

--- shop/admin.py
+++ shop/admin.py
@@ -1,1 +1,16 @@
 
+from django.contrib import admin
+from .models import Shop
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'status', 'created_date')  # Columns in the list view
+    list_filter = ('status', 'created_date')  # Filters in the list view
+    search_fields = ('name', 'email', 'phone')  # Searchable fields
+    readonly_fields = ('created_date',)  # Fields that cannot be edited
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'email', 'phone', 'password', 'status', 'logo', 'x_gis', 'y_gis', 'reset_code', 'created_date')
+        }),
+    )
+
+admin.site.register(Shop, ShopAdmin)
